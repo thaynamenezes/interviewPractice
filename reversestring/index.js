bf5ac7e9c0604d1619dac6ad6fb1f704c531c457
Thayna Santana <thaynamenezes01@gmail.com>
2019-07-21 18:32:18 -0400

    practiced reverse string

---
+++ reversestring/index.js
@@ -0,0 +1,41 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    //SOLUTION 1
+//     let reversed = '';
+//     for(let i = str.length - 1; i > -1; i--){
+//       reversed += str[i]
+//     }
+//   return reversed  
+
+        // another way of for loop
+    // let reversed = '';
+    //  for ( let character of str) {
+    //    reversed = character + reversed;
+    //  }
+    // return reversed  
+
+
+    //SOLUTION 2
+//     arr = str.split('');
+//     arr.reverse();
+//     return arr.join('');
+        //clean version 
+    // return str.split('').reverse().join('');
+
+    //SOLUTION 3
+    return str.split('')
+    .reduce((reversed, character) => {
+        return character + reversed;
+    }, '');
+}
+console.log(reverse('Jonas'));
+
+
+module.exports = reverse;
